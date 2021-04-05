fd0ef24bfea408e07b79aa3438e733271257a901
victor kigen <vickigen@gmail.com>
2021-04-05 13:35:05 +0300

    add initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { GithubComponent } from "./components/github/github.component";
+
+const routes: Routes = [{ path: "github", component: GithubComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
